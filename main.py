86128a3e5f372e04aa1c541f8975433321968b79
Ryan Valler <rwv1293@gmail.com>
2022-04-20 15:16:43 -0400

    rebuilt to aid in readability and functionality. Added helper methods for total calculator to calc individual item cost & process tax based on state.

--- main.py
+++ main.py
@@ -26,19 +26,50 @@ class item:
     category: Category
 
 
-def total_calculator(shopping_cart, a_state):
-    tax_rate = 0
-    total = 0
-    cloth_total = 0
-    wic_total = 0
-    other_total = 0
-    if a_state == "Massachusetts":
-        tax_rate = 0.0675
-    elif a_state == "New Hampshire":
-        tax_rate = 0
-    elif a_state == "Maine":
+def get_taxes(state_):
+    if state_ == "Massachusetts":
+        tax_rate = 0.0625
+    elif state_ == "Maine":
         tax_rate = 0.0550
-    print(tax_rate)
+    else:
+        tax_rate = 0
+    return tax_rate
+
+
+def calc_item_cost(state_, item_):
+    cat = item_.category
+    tax_rate = get_taxes(state_)
+    sub_total = 0
+
+    if cat == Category.other:
+        price = item_.price
+        total_price = price * item_.quantity + (price * tax_rate)
+        sub_total += round(total_price, 2)
+    elif cat == Category.wic:
+        price = item_.price
+        total_price = price * item_.quantity
+        sub_total += round(total_price, 2)
+    elif cat == Category.clothing:
+        price = item_.price
+        if state_ == "Massachusetts" and price >= 175:
+            total_price = price * item_.quantity + (price * tax_rate)
+            sub_total += round(total_price, 2)
+        else:
+            total_price = price * item_.quantity
+            sub_total += round(total_price, 2)
+
+    return sub_total
+
+
+def total_calculator(shopping_cart, a_state):
+    grand_total = 0
+    for item_ in shopping_cart:
+        if item_.price > 0:
+            grand_total += calc_item_cost(a_state, item_)
+        else:
+            ValueError(f"The item {item_.name} has no associated cost. Refunds cannot be processed at this time. ")
+    return round(grand_total, 2)
+
 
 def main():
     state_list = ["MASSACHUSETTS", "MAINE", "NEW HAMPSHIRE", "MA", "ME", "NH"]
@@ -53,14 +84,14 @@ def main():
         state_ = "New Hampshire"
 
     shopping_list = []
-    cookie = item("Oreo", 3.99, 2, Category.wic)
+    cookie = item("Oreo", 3.99, 1, Category.wic)
     shopping_list.append(cookie)
-    shirt_1 = item("V-Neck", 8.00, 3, Category.clothing)
-    shopping_list.append(shirt_1)
-    pants_1 = item("Slacks", 15.99, 1, Category.clothing)
-    shopping_list.append(pants_1)
-    vacuum = item("Hoover", 49.99, 1, Category.other)
-    shopping_list.append(vacuum)
+    # shirt_1 = item("Suit", 200.0, 1, Category.clothing)
+    # shopping_list.append(shirt_1)
+    # pants_1 = item("Slacks", 15.99, 1, Category.clothing)
+    # shopping_list.append(pants_1)
+    # vacuum = item("Hoover", 49.99, 1, Category.other)
+    # shopping_list.append(vacuum)
 
     print(total_calculator(shopping_list, state_))
 
